ab1cc985dbe1478d73627770f25aed44338ff839
thiagowfer <thiagowfer@gmail.com>
2019-09-23 18:22:01 -0300

    Update client_id

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$().ready(function() {
+   $('#searchUser').on('keyup', function(e){
+       let username = e.target.value;
+
+       // Make request to github
+       $.ajax({
+           url: 'https://api.github.com/users/' + username,
+           data: {
+               client_id: '34ea6e924809745ff2f9',
+               client_secret: 'b56d8f2265dabc804c854cc73770ff43c769f32c'
+           }
+       });
+   })
+});
