23b05b847b46c6ad4d1214e95132bfb234986d43
thiagowfer <thiagowfer@gmail.com>
2019-09-23 18:45:20 -0300

    Showing username and user image when type something

--- js/main.js
+++ js/main.js
@@ -10,7 +10,22 @@ $().ready(function() {
                client_secret: 'b56d8f2265dabc804c854cc73770ff43c769f32c'
            }
        }).done(function(user){
-        console.log(user);
+        $('#profile').html(`
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="">
+                <div class="card-body">
+                <h5 class="card-title">${user.name}</h5>
+                <div class="row">
+                    <div class="col-md-3">
+                        <img style="width:15rem" src="${user.avatar_url}" class="thumbnail">
+                    </div>
+                    <div class="col-md-9">
+                    
+                    </div>
+                </div>
+                </div>
+            </div>
+        `);
        });
    })
 });
