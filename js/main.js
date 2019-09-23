0ccc5dba5311877a2a30898d27989b7a2b15d4b5
thiagowfer <thiagowfer@gmail.com>
2019-09-23 19:19:46 -0300

    Project Finished

--- js/main.js
+++ js/main.js
@@ -1,37 +1,73 @@
-$().ready(function() {
-   $('#searchUser').on('keyup', function(e){
-       let username = e.target.value;
-
-       // Make request to github
-       $.ajax({
-           url: 'https://api.github.com/users/' + username,
-           data: {
-               client_id: '34ea6e924809745ff2f9',
-               client_secret: 'b56d8f2265dabc804c854cc73770ff43c769f32c'
-           }
-       }).done(function(user){
-        // Show profile info
-        $('#profile').html(`
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="">
-                <div class="card-body">
-                <h5 class="card-title">${user.name}</h5>
+$(document).ready(function(){
+    $('#searchUser').on('keyup', function(e){
+      let username = e.target.value;
+  
+      // Make request to Github
+      $.ajax({
+          url:'https://api.github.com/users/'+username,
+          data:{
+            client_id:'b9315bcd5a07fcd759d8',
+            client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
+          }
+      }).done(function(user){
+        $.ajax({
+          url:'https://api.github.com/users/'+username+'/repos',
+          data:{
+            client_id:'b9315bcd5a07fcd759d8',
+            client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4',
+            sort: 'created: asc',
+            per_page: 5
+          }
+        }).done(function(repos){
+          $.each(repos, function(index, repo){
+            $('#repos').append(`
+              <div class="card">
                 <div class="row">
-                    <div class="col-md-3">
-                        <img style="width:15rem" src="${user.avatar_url}" class="thumbnail">
-                        <a target="_blank" class="btn btn-primary" href="${user.html_url}">Show Profile<a/>
-                    </div>
-                    <div class="col-md-9">
-                    <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-                    <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-                    <span class="badge badge-success">Followers: ${user.followers}</span>
-                    <span class="badge badge-danger">Following: ${user.following}</span>
-                    <br/><br/>
-                    </div>
+                  <div class="col-md-7">
+                    <strong>${repo.name}</strong>: ${repo.description}
+                  </div>
+                  <div class="col-md-3">
+                    <span class="badge badge-dark">Forks: ${repo.forks_count}</span>
+                    <span class="badge badge-primary">Watchers: ${repo.watchers_count}</span>
+                    <span class="badge badge-success">Stars: ${repo.stargazers_count}</span>
+                  </div>
+                  <div class="col-md-2">
+                    <a href="${repo.html_url}" target="_blank" class="btn btn-dark">Repo Page</a>
+                  </div>
                 </div>
+              </div>
+            `);
+          });
+        });
+        $('#profile').html(`
+          <div class="card border-primary mb-3" style="max-width: 100rem;">
+            <div class="card-header"><h3>${user.name}</h3></div>
+            <div class="card-body">
+              <div class="row">
+              <div class="col-md-3">
+                <img class="img-thumbnail avatar" src="${user.avatar_url}">
+                <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+              </div>
+              <div class="col-md-9">
+                <span class="badge badge-dark">Public Repos: ${user.public_repos}</span>
+                <span class="badge badge-primary">Public Gists: ${user.public_gists}</span>
+                <span class="badge badge-success">Followers: ${user.followers}</span>
+                <span class="badge badge-info">Following: ${user.following}</span>
+                <br><br>
+                <ul class="list-group">
+                  <li class="list-group-item">Company: ${user.company}</li>
+                  <li class="list-group-item">Website/blog: <a href="${user.blog}" target="_blank">${user.blog}</a></li>
+                  <li class="list-group-item">Location: ${user.location}</li>
+                  <li class="list-group-item">Member Since: ${user.created_at}</li>
+                </ul>
                 </div>
+              </div>
             </div>
-        `);
-       });
-   })
-});
+          </div>
+          <h3 class="page-header">Latest Repos</h3>
+          <div id="repos"></div>
+          `);
+      });
+    });
+  });
+  
